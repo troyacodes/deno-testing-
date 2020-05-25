389648553e5a381d81a317f031fca2264e5d4d11
Michael <michaeltroya13@gmail.com>
2020-05-25 14:01:19 -0400

    add route to fetch all todos

---
+++ controllers/todos/post.js
@@ -0,0 +1,18 @@
+import { FILE_PATH } from '../../config.js';
+
+export default async ({ response }) => {
+  const decoder = new TextDecoder();
+
+  try {
+    const data = await Deno.readFile(FILE_PATH);
+    const todos = await JSON.parse(decoder.decode(data));
+
+    response.status = 200;
+    response.body = { status: 'success', todos };
+
+    console.log(todos);
+  } catch (error) {
+    response.status = 500;
+    response.body = { status: 'failed', error };
+  }
+};
--- router.js
+++ router.js
@@ -1,9 +1,13 @@
 import { Router } from 'https://deno.land/x/oak/mod.ts';
 
+import getTodos from './controllers/todos/get.js';
+
 const router = new Router();
 
 router.get('/', ({ response }) => {
   response.body = 'Hello';
 });
+
+router.get('/todos', getTodos);
 
 export default router;
